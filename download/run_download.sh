dbb3f4f90fcf629ba03fd3383fc8e17d6747b131
Felipe Nunes <felipe.nunes@neo4j.com>
2022-06-14 11:07:33 -0300

    no message

--- download/run_download.sh
+++ download/run_download.sh
@@ -1,1 +1,3 @@
-curl http://200.152.38.155/CNPJ/K3241.K03200Y0.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/teste.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y0.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOCSV01.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y1.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOCSV02.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y2.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOCSV02.zip
