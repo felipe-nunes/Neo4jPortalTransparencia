b5d108e42f8b0b900a45c4fe6d9d11795fa739e9
Felipe Nunes <felipe-nunes@users.noreply.github.com>
2022-06-15 10:15:44 -0300

    Update run_download.sh

--- download/run_download.sh
+++ download/run_download.sh
@@ -1,53 +1,53 @@
 # DATA DA ULTIMA EXTRACAO DESTES ARQUIVOS : 17/05/2022
 # https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/dados-publicos-cnpj
 # DOWNLOAD DE DADOS PUBLICOS DE EMPRESAS
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y0.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/EMPRESASCSV01.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y1.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/EMPRESASCSV02.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y2.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/EMPRESASCSV03.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y3.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/EMPRESASCSV04.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y4.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/EMPRESASCSV05.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y5.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/EMPRESASCSV06.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y6.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/EMPRESASCSV07.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y7.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/EMPRESASCSV08.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y8.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/EMPRESASCSV09.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y9.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/EMPRESASCSV10.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y0.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/EMPRESASCSV01.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y1.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/EMPRESASCSV02.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y2.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/EMPRESASCSV03.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y3.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/EMPRESASCSV04.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y4.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/EMPRESASCSV05.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y5.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/EMPRESASCSV06.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y6.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/EMPRESASCSV07.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y7.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/EMPRESASCSV08.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y8.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/EMPRESASCSV09.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y9.D20514.EMPRECSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/EMPRESASCSV10.zip
 # DOWNLOAD DE DADOS PUBLICOS DE ESTABELECIMENTOS
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y0.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ESTABELECIMENTOSCSV01.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y1.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ESTABELECIMENTOSCSV02.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y2.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ESTABELECIMENTOSCSV03.zip
-# curl http://200.152.38.155/CNPJ/K3241.K03200Y3.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ESTABELECIMENTOSCSV04.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y4.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ESTABELECIMENTOSCSV05.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y5.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ESTABELECIMENTOSCSV06.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y6.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ESTABELECIMENTOSCSV07.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y7.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ESTABELECIMENTOSCSV08.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y8.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ESTABELECIMENTOSCSV09.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y9.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ESTABELECIMENTOSCSV10.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y0.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ESTABELECIMENTOSCSV01.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y1.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ESTABELECIMENTOSCSV02.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y2.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ESTABELECIMENTOSCSV03.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y3.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ESTABELECIMENTOSCSV04.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y4.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ESTABELECIMENTOSCSV05.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y5.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ESTABELECIMENTOSCSV06.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y6.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ESTABELECIMENTOSCSV07.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y7.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ESTABELECIMENTOSCSV08.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y8.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ESTABELECIMENTOSCSV09.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y9.D20514.ESTABELE.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ESTABELECIMENTOSCSV10.zip
 # DOWNLOAD DE DADOS PUBLICOS DE SOCIOS
-curl http://200.152.38.155/CNPJ/K3241.K03200Y0.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOSCSV01.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y1.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOSCSV02.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y2.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOSCSV03.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y3.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOSCSV04.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y4.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOSCSV05.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y5.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOSCSV06.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y6.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOSCSV07.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y7.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOSCSV08.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y8.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOSCSV09.zip
-curl http://200.152.38.155/CNPJ/K3241.K03200Y9.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SOCIOSCSV10.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y0.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SOCIOSCSV01.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y1.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SOCIOSCSV02.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y2.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SOCIOSCSV03.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y3.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SOCIOSCSV04.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y4.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SOCIOSCSV05.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y5.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SOCIOSCSV06.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y6.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SOCIOSCSV07.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y7.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SOCIOSCSV08.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y8.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SOCIOSCSV09.zip
+curl http://200.152.38.155/CNPJ/K3241.K03200Y9.D20514.SOCIOCSV.zip | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SOCIOSCSV10.zip
 # DOWNLOAD DE DADOS DE INFORMACOES SOBRE O SIMPLES NACIONAL E MEI
-curl "http://200.152.38.155/CNPJ/F.K03200$W.SIMPLES.CSV.D20514.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SIMPLES_MEI.zip
+curl "http://200.152.38.155/CNPJ/F.K03200$W.SIMPLES.CSV.D20514.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SIMPLES_MEI.zip
 # DOWNLOAD DA TABELA DE ATRIBUTO CNAE
-curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.CNAECSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SIMPLES_MEI.zip
+curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.CNAECSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SIMPLES_MEI.zip
 # DOWNLOAD DA TABELA DE MOTIVO DE SITUACAO CADASTRAL
-curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.MOTICSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/SITUACAO_CADASTRAL.zip
+curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.MOTICSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/SITUACAO_CADASTRAL.zip
 # DOWNLOAD DE DADOS DE ATRIBUTO MUNICIPIO
-curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.MUNICCSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ATRIBUTO_MUNICIPIO.zip
+curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.MUNICCSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ATRIBUTO_MUNICIPIO.zip
 # DOWNLOAD DE DADOS DE ATRIBUTO NATUREZA JURIDICA
-curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.NATJUCSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/NATUREZA_JURIDICA.zip
+curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.NATJUCSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/NATUREZA_JURIDICA.zip
 # DOWNLOAD DE DADOS DE ATRIBUTO PAIS 
-curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.PAISCSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/ATRIBUTO_PAIS.zip
+curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.PAISCSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/ATRIBUTO_PAIS.zip
 # DOWNLOAD DE DADOS DE ATRIBUTO QUALIFICACAO DOS SOCIOS 
-curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.QUALSCSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/QUALIFICACAO_SOCIOS.zip
+curl "http://200.152.38.155/CNPJ/F.K03200$Z.D20514.QUALSCSV.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/1_DOCUMENTATION/QUALIFICACAO_SOCIOS.zip
 # DOWNLOAD DE LAYOUT DOS ARQUIVOS DE DADOS DE SOCIOS
 curl "https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/arquivos/leiaute-dos-arquivos.pdf" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/LAYOUT_SOCIOS.pdf
 # DOWNLOAD DE REGIMES TRIBUTARIOS
-curl "http://200.152.38.155/CNPJ/anual/Dados%20Abertos%20S%c3%adtio%20RFB%20Extracao%2020.10.2021.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/REGIMES_TRIBUTARIOS.ZIP
+curl "http://200.152.38.155/CNPJ/anual/Dados%20Abertos%20S%c3%adtio%20RFB%20Extracao%2020.10.2021.zip" | gsutil cp - gs://neo4j_nunes/dp_socios_dataset/0_RAW_DATASET/REGIMES_TRIBUTARIOS.ZIP
