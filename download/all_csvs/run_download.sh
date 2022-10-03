623c1ba5f1620b14d1bb5ff51ed536683d6e7b29
Felipe Nunes <felipe.nunes@neo4j.com>
2022-10-03 10:29:49 -0300

    no message

---
+++ download/all_csvs/run_download.sh
@@ -0,0 +1,53 @@
+# DATA DA ULTIMA EXTRACAO DESTES ARQUIVOS : 23/09/2022
+# https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/dados-publicos-cnpj
+# DOWNLOAD DE DADOS PUBLICOS DE EMPRESAS
+wget http://200.152.38.155/CNPJ/Empresas0.zip 
+wget http://200.152.38.155/CNPJ/Empresas1.zip 
+wget http://200.152.38.155/CNPJ/Empresas2.zip 
+wget http://200.152.38.155/CNPJ/Empresas3.zip 
+wget http://200.152.38.155/CNPJ/Empresas4.zip 
+wget http://200.152.38.155/CNPJ/Empresas5.zip 
+wget http://200.152.38.155/CNPJ/Empresas6.zip 
+wget http://200.152.38.155/CNPJ/Empresas7.zip 
+wget http://200.152.38.155/CNPJ/Empresas8.zip 
+wget http://200.152.38.155/CNPJ/Empresas9.zip 
+# DOWNLOAD DE DADOS PUBLICOS DE ESTABELECIMENTOS
+wget http://200.152.38.155/CNPJ/Estabelecimentos0.zip 
+wget http://200.152.38.155/CNPJ/Estabelecimentos1.zip 
+wget http://200.152.38.155/CNPJ/Estabelecimentos2.zip 
+wget http://200.152.38.155/CNPJ/Estabelecimentos3.zip 
+wget http://200.152.38.155/CNPJ/Estabelecimentos4.zip 
+wget http://200.152.38.155/CNPJ/Estabelecimentos5.zip 
+wget http://200.152.38.155/CNPJ/Estabelecimentos6.zip 
+wget http://200.152.38.155/CNPJ/Estabelecimentos7.zip 
+wget http://200.152.38.155/CNPJ/Estabelecimentos8.zip 
+wget http://200.152.38.155/CNPJ/Estabelecimentos9.zip 
+# DOWNLOAD DE DADOS PUBLICOS DE SOCIOS
+wget http://200.152.38.155/CNPJ/Socios0.zip 
+wget http://200.152.38.155/CNPJ/Socios1.zip 
+wget http://200.152.38.155/CNPJ/Socios2.zip 
+wget http://200.152.38.155/CNPJ/Socios3.zip 
+wget http://200.152.38.155/CNPJ/Socios4.zip 
+wget http://200.152.38.155/CNPJ/Socios5.zip 
+wget http://200.152.38.155/CNPJ/Socios6.zip 
+wget http://200.152.38.155/CNPJ/Socios7.zip 
+wget http://200.152.38.155/CNPJ/Socios8.zip 
+wget http://200.152.38.155/CNPJ/Socios9.zip 
+# DOWNLOAD DE DADOS DE INFORMACOES SOBRE O SIMPLES NACIONAL E MEI
+wget http://200.152.38.155/CNPJ/Simples.zip 
+# DOWNLOAD DA TABELA DE ATRIBUTO CNAE
+wget http://200.152.38.155/CNPJ/Cnaes.zip 
+# DOWNLOAD DA TABELA DE MOTIVO DE SITUACAO CADASTRAL
+wget http://200.152.38.155/CNPJ/Motivos.zip 
+# DOWNLOAD DE DADOS DE ATRIBUTO MUNICIPIO
+wget http://200.152.38.155/CNPJ/Municipios.zip 
+# DOWNLOAD DE DADOS DE ATRIBUTO NATUREZA JURIDICA
+wget http://200.152.38.155/CNPJ/Naturezas.zip 
+# DOWNLOAD DE DADOS DE ATRIBUTO PAIS 
+wget http://200.152.38.155/CNPJ/Paises.zip 
+# DOWNLOAD DE DADOS DE ATRIBUTO QUALIFICACAO DOS SOCIOS 
+wget http://200.152.38.155/CNPJ/Qualificacoes.zip 
+# DOWNLOAD DE LAYOUT DOS ARQUIVOS DE DADOS DE SOCIOS
+wget https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/arquivos/novolayoutdosdadosabertosdocnpj-dez2021.pdf 
+# DOWNLOAD DE REGIMES TRIBUTARIOS
+wget http://200.152.38.155/CNPJ/anual/Dados%20Abertos%20S%c3%adtio%20RFB%20Extracao%2020.10.2021.zip 
